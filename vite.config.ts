7b549970029bfe632bcda3c0c4b07fe65b1415da
ShakePeng <634676514@qq.com>
2021-05-20 10:29:59 +0800

    @vitejs/plugin-legacy 兼容esmoudle

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 let root = process.cwd()
 // https://vitejs.dev/config/
 const resolve = (dir: string) => path.join(__dirname, dir)
@@ -8,7 +9,12 @@ const resolve = (dir: string) => path.join(__dirname, dir)
 export default ({ command, mode }) => {
     let env = loadEnv(mode, root)
     return defineConfig({
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            legacy({
+                targets: ['defaults', 'not IE 11']
+            })
+        ],
         base: env.VITE_BASE_URL,
         resolve: {
             alias: {
